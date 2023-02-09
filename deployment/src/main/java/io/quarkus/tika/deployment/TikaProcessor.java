5f9d1037ee7a1161232f23f840a298afbcb53d22
Tomaz Podgorsek <tomaz.podgorsek@povio.com>
2023-02-09 13:38:45 +0100

    Import specific tika modules instead of whole standard package

--- deployment/src/main/java/io/quarkus/tika/deployment/TikaProcessor.java
+++ deployment/src/main/java/io/quarkus/tika/deployment/TikaProcessor.java
@@ -1,15 +1,8 @@
 package io.quarkus.tika.deployment;
 
 import java.lang.reflect.Method;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.LinkedList;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 import java.util.Map.Entry;
-import java.util.Optional;
-import java.util.Set;
 import java.util.function.Function;
 import java.util.function.Predicate;
 import java.util.stream.Collectors;
