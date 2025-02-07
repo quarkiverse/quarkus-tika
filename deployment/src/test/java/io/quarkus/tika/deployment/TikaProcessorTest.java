46cbc208ad05cbecb629f224bedc85b3142d1880
George Gastaldi <gegastaldi@gmail.com>
2025-02-07 12:04:36 -0300

    Refactor parser tests and enhance dependency clarity
    ```
    - Add `assertj-core` library for enhanced testing capabilities.
    - Refactor parser tests to use expressive and maintainable assertions with `assertThat` and explicit parser lists.
    ```

--- deployment/src/test/java/io/quarkus/tika/deployment/TikaProcessorTest.java
+++ deployment/src/test/java/io/quarkus/tika/deployment/TikaProcessorTest.java
@@ -1,5 +1,6 @@
 package io.quarkus.tika.deployment;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.junit.jupiter.api.Assertions.fail;
@@ -108,13 +109,98 @@ public class TikaProcessorTest {
 
     @Test
     public void testAllSupportedParserNames() throws Exception {
-        assertEquals(74, getParserNames(null, null).size());
+        String[] expected = { "org.apache.tika.parser.microsoft.xml.WordMLParser",
+                "org.apache.tika.parser.microsoft.rtf.RTFParser",
+                "org.apache.tika.parser.audio.MidiParser",
+                "org.apache.tika.parser.external.CompositeExternalParser",
+                "org.apache.tika.parser.warc.WARCParser", "org.apache.tika.parser.wacz.WACZParser",
+                "org.apache.tika.parser.microsoft.ooxml.OOXMLParser", "org.apache.tika.parser.csv.TextAndCSVParser",
+                "org.apache.tika.parser.apple.PListParser", "org.apache.tika.parser.asm.ClassParser",
+                "org.apache.tika.parser.font.AdobeFontMetricParser", "org.apache.tika.parser.xliff.XLIFF12Parser",
+                "org.apache.tika.parser.html.HtmlParser", "org.apache.tika.parser.font.TrueTypeParser",
+                "org.apache.tika.parser.microsoft.WMFParser", "org.apache.tika.parser.http.HttpParser",
+                "org.apache.tika.parser.xliff.XLZParser", "org.apache.tika.parser.image.HeifParser",
+                "org.apache.tika.parser.image.TiffParser", "org.apache.tika.parser.dif.DIFParser",
+                "org.apache.tika.parser.microsoft.OfficeParser", "org.apache.tika.parser.hwp.HwpV5Parser",
+                "org.apache.tika.parser.feed.FeedParser", "org.apache.tika.parser.microsoft.OldExcelParser",
+                "org.apache.tika.parser.pkg.PackageParser", "org.apache.tika.parser.mp4.MP4Parser",
+                "org.apache.tika.parser.dwg.DWGParser", "org.apache.tika.parser.mif.MIFParser",
+                "org.apache.tika.parser.iptc.IptcAnpaParser", "org.apache.tika.parser.microsoft.EMFParser",
+                "org.apache.tika.parser.wordperfect.WordPerfectParser", "org.apache.tika.parser.sas.SAS7BDATParser",
+                "org.apache.tika.parser.microsoft.JackcessParser", "org.apache.tika.parser.mbox.MboxParser",
+                "org.apache.tika.parser.image.WebPParser", "org.apache.tika.parser.image.ICNSParser",
+                "org.apache.tika.parser.odf.FlatOpenDocumentParser", "org.apache.tika.parser.crypto.Pkcs7Parser",
+                "org.apache.tika.parser.microsoft.MSOwnerFileParser", "org.apache.tika.parser.xml.DcXMLParser",
+                "org.apache.tika.parser.image.JXLParser", "org.apache.tika.parser.code.SourceCodeParser",
+                "org.apache.tika.parser.apple.AppleSingleFileParser", "org.apache.tika.parser.indesign.IDMLParser",
+                "org.apache.tika.parser.microsoft.activemime.ActiveMimeParser",
+                "org.apache.tika.parser.microsoft.TNEFParser", "org.apache.tika.parser.dgn.DGN8Parser",
+                "org.apache.tika.parser.microsoft.ooxml.xwpf.ml2006.Word2006MLParser",
+                "org.apache.tika.parser.image.BPGParser", "org.apache.tika.parser.dbf.DBFParser",
+                "org.apache.tika.parser.microsoft.pst.OutlookPSTParser",
+                "org.apache.tika.parser.iwork.iwana.IWork18PackageParser", "org.apache.tika.parser.video.FLVParser",
+                "org.apache.tika.parser.prt.PRTParser", "org.apache.tika.parser.pkg.RarParser",
+                "org.apache.tika.parser.tmx.TMXParser", "org.apache.tika.parser.iwork.IWorkPackageParser",
+                "org.apache.tika.parser.microsoft.onenote.OneNoteParser",
+                "org.apache.tika.parser.iwork.iwana.IWork13PackageParser",
+                "org.apache.tika.parser.microsoft.xml.SpreadsheetMLParser",
+                "org.apache.tika.parser.microsoft.chm.ChmParser",
+                "org.apache.tika.parser.executable.ExecutableParser", "org.apache.tika.parser.image.JpegParser",
+                "org.apache.tika.parser.image.PSDParser", "org.apache.tika.parser.xml.FictionBookParser",
+                "org.apache.tika.parser.audio.AudioParser", "org.apache.tika.parser.epub.EpubParser",
+                "org.apache.tika.parser.image.ImageParser", "org.apache.tika.parser.pdf.PDFParser",
+                "org.apache.tika.parser.odf.OpenDocumentParser", "org.apache.tika.parser.mp3.Mp3Parser",
+                "org.apache.tika.parser.wordperfect.QuattroProParser", "org.apache.tika.parser.crypto.TSDParser",
+                "org.apache.tika.parser.ocr.TesseractOCRParser" };
+        assertThat(getParserNames(null, null)).containsExactlyInAnyOrder(expected);
     }
 
     @Test
     public void testSupportedParserNamesWithTikaConfigPath() throws Exception {
-        Set<String> names = getParserNames("tika-config.xml", "pdf");
-        assertEquals(74, names.size());
+        String[] expected = { "org.apache.tika.parser.microsoft.xml.WordMLParser",
+                "org.apache.tika.parser.microsoft.rtf.RTFParser",
+                "org.apache.tika.parser.audio.MidiParser",
+                "org.apache.tika.parser.external.CompositeExternalParser",
+                "org.apache.tika.parser.warc.WARCParser", "org.apache.tika.parser.wacz.WACZParser",
+                "org.apache.tika.parser.microsoft.ooxml.OOXMLParser", "org.apache.tika.parser.csv.TextAndCSVParser",
+                "org.apache.tika.parser.apple.PListParser", "org.apache.tika.parser.asm.ClassParser",
+                "org.apache.tika.parser.font.AdobeFontMetricParser", "org.apache.tika.parser.xliff.XLIFF12Parser",
+                "org.apache.tika.parser.html.HtmlParser", "org.apache.tika.parser.font.TrueTypeParser",
+                "org.apache.tika.parser.microsoft.WMFParser", "org.apache.tika.parser.http.HttpParser",
+                "org.apache.tika.parser.xliff.XLZParser", "org.apache.tika.parser.image.HeifParser",
+                "org.apache.tika.parser.image.TiffParser", "org.apache.tika.parser.dif.DIFParser",
+                "org.apache.tika.parser.microsoft.OfficeParser", "org.apache.tika.parser.hwp.HwpV5Parser",
+                "org.apache.tika.parser.feed.FeedParser", "org.apache.tika.parser.microsoft.OldExcelParser",
+                "org.apache.tika.parser.pkg.PackageParser", "org.apache.tika.parser.mp4.MP4Parser",
+                "org.apache.tika.parser.dwg.DWGParser", "org.apache.tika.parser.mif.MIFParser",
+                "org.apache.tika.parser.iptc.IptcAnpaParser", "org.apache.tika.parser.microsoft.EMFParser",
+                "org.apache.tika.parser.wordperfect.WordPerfectParser", "org.apache.tika.parser.sas.SAS7BDATParser",
+                "org.apache.tika.parser.microsoft.JackcessParser", "org.apache.tika.parser.mbox.MboxParser",
+                "org.apache.tika.parser.image.WebPParser", "org.apache.tika.parser.image.ICNSParser",
+                "org.apache.tika.parser.odf.FlatOpenDocumentParser", "org.apache.tika.parser.crypto.Pkcs7Parser",
+                "org.apache.tika.parser.microsoft.MSOwnerFileParser", "org.apache.tika.parser.xml.DcXMLParser",
+                "org.apache.tika.parser.image.JXLParser", "org.apache.tika.parser.code.SourceCodeParser",
+                "org.apache.tika.parser.apple.AppleSingleFileParser", "org.apache.tika.parser.indesign.IDMLParser",
+                "org.apache.tika.parser.microsoft.activemime.ActiveMimeParser",
+                "org.apache.tika.parser.microsoft.TNEFParser", "org.apache.tika.parser.dgn.DGN8Parser",
+                "org.apache.tika.parser.microsoft.ooxml.xwpf.ml2006.Word2006MLParser",
+                "org.apache.tika.parser.image.BPGParser", "org.apache.tika.parser.dbf.DBFParser",
+                "org.apache.tika.parser.microsoft.pst.OutlookPSTParser",
+                "org.apache.tika.parser.iwork.iwana.IWork18PackageParser", "org.apache.tika.parser.video.FLVParser",
+                "org.apache.tika.parser.prt.PRTParser", "org.apache.tika.parser.pkg.RarParser",
+                "org.apache.tika.parser.tmx.TMXParser", "org.apache.tika.parser.iwork.IWorkPackageParser",
+                "org.apache.tika.parser.microsoft.onenote.OneNoteParser",
+                "org.apache.tika.parser.iwork.iwana.IWork13PackageParser",
+                "org.apache.tika.parser.microsoft.xml.SpreadsheetMLParser",
+                "org.apache.tika.parser.microsoft.chm.ChmParser",
+                "org.apache.tika.parser.executable.ExecutableParser", "org.apache.tika.parser.image.JpegParser",
+                "org.apache.tika.parser.image.PSDParser", "org.apache.tika.parser.xml.FictionBookParser",
+                "org.apache.tika.parser.audio.AudioParser", "org.apache.tika.parser.epub.EpubParser",
+                "org.apache.tika.parser.image.ImageParser", "org.apache.tika.parser.pdf.PDFParser",
+                "org.apache.tika.parser.odf.OpenDocumentParser", "org.apache.tika.parser.mp3.Mp3Parser",
+                "org.apache.tika.parser.wordperfect.QuattroProParser", "org.apache.tika.parser.crypto.TSDParser",
+                "org.apache.tika.parser.ocr.TesseractOCRParser" };
+        assertThat(getParserNames("tika-config.xml", "pdf")).containsExactlyInAnyOrder(expected);
     }
 
     @Test
